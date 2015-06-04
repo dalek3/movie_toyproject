190073baf62299c2a815cc1242123004af156889
Jang <lina1318@gmail.com>
2015-06-04 22:11:00 +0900

    jade

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+exports.index = function(req, res){
+  res.render('index');
+};
+
+exports.register = function(req, res){
+  res.render('register');
+};
+
+exports.login = function(req, res){
+	res.render('login');
+}
--- server.js
+++ server.js
@@ -1,56 +1,54 @@
 //모듈을 추출합니다.
-var http = require('http'),
-    express = require('express');
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path');
 
 //웹 서버를 생성합니다.
 var app = express();
-app.use(express.static('public'));
-app.use(express.bodyParser());
-app.use(app.router);
 
-app.get('/register', function (request, response) {
-    var output = '';
-    output += '<form action="/" class="form-horizontal" method="post">'
-    output += '     <div class="form-group">'
-    output += '         <div class="header">'
-    output += '             <h2>Sign Up</h2>'
-    output += '         </div>'
-    output += '         <div class="content">'
-    output += '             <label for="email">E-mail</label>'
-    output += '             <input type="email" class="form-control" id="useremail" placeholder="Email" required/>'
-    output += '             <label>Password</label>'
-    output += '             <input type="password" class="form-control" id="password" placeholder="Password" />'
-    output += '             <label>Password Check</label>'
-    output += '             <input type="password" class="form-control" id="password2" placeholder="Password Check">'
-    output += '         </div>'
-    output += '         <div class="footer">'
-    output += '             <button type="submit" class="btn btn-primary">Sign Up</button>'
-    output += '         </div>'
-    output += '     </div>'
-    output += '</form>'
-    response.send(output);
+app.configure(function(){
+  app.set('port', process.env.PORT || 52273);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/register', routes.register);
+app.get('/login', routes.login);
+/*
 
 app.get('/login', function (request, response) {
     var output = '';
-    output += '   <form action="/" method="post" class="form-horizontal">'
-    output += '       <div class="form-group">'
-    output += '               <div class="header">'
-    output += '                   <h2>Login</h2>'
-    output += '               </div>'
-    output += '               <div class="content">'
-    output += '                   <label>E-mail</label>'
-    output += '                   <input type="email" class="form-control" id="useremail" placeholder="Email" />'
-    output += '                   <label>Password</label>'
-    output += '                   <input type="password" class="form-control" id="password" placeholder="Password" />'
-    output += '                   <input type="checkbox" id="remember-me" value="1" />'
-    output += '                   <label class="string optional" for="user_remember_me">기억하기</label>'
-    output += '               </div>'
-    output += '               <div class="footer">'
-    output += '                   <button type="submit" class="btn btn-primary">Login</button>'
-    output += '               </div>'
-    output += '       </div>'
-    output += '   </form>'
+       <form action="/" method="post" class="form-horizontal">
+           <div class="form-group">
+                   <div class="header">
+                       <h2>Login</h2>
+                   </div>
+                   <div class="content">
+                       <label>E-mail</label>
+                       <input type="email" class="form-control" id="useremail" placeholder="Email" />
+                      <label>Password</label>
+                       <input type="password" class="form-control" id="password" placeholder="Password" />
+                       <input type="checkbox" id="remember-me" value="1" />
+                       <label class="string optional" for="user_remember_me">기억하기</label>
+                   </div>
+                   <div class="footer">
+                       <button type="submit" class="btn btn-primary">Login</button>
+                   </div>
+           </div>
+       </form>
     response.send(output);
 });
 
@@ -76,8 +74,17 @@ app.get('/mv', function (request, response) {
     output += '   </form>'
     response.send(output);
 });
+*/
 
 //웹서버를 실행합니다.
-var server = app.listen(52273, function () {
-    console.log('Server Running at ' + server.address().port);
-});
+var server = http.createServer(app);
+
+//module.parent는 현재 실행된 모듈 프로세스의 부모가 있는 지 확인할 수 있는 속성입니다. 이를 이용하여 최초 실행되는 프로세스만 서버를 실행합니다.
+if (!module.parent) {
+	server.listen(app.get('port'), function(){
+	  console.log("Express server listening on port " + app.get('port'));
+	});
+}
+else {
+	module.exports = server;
+}
