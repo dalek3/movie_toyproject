4594d8eeba49a69033c43ca58da6acb10286ab79
Jang <lina1318@gmail.com>
2015-06-07 09:56:23 +0900

    영화정보 출력modal

--- public/js/test.js
+++ public/js/test.js
@@ -7,13 +7,15 @@ $(function () {
             var html = '';
             $.each(data, function (index, item) {
                 if(index === 0){
-                    html += '<a href="#" id=test class="thumbnail col-md-5 col-sm-5 pull-left">';
+                    html += '<a href="#" id=test class="thumbnail col-md-4 col-sm-4 col-xs-4 pull-left">';
                     html += '<img src="' + item.imgpath + '"style="width: 100%; height: auto;">';
                     html += '</a>'
-                    $('#title').html(item.name)
-                    $('#director').html(item.director)
-                    $('#actor1').html(item.actor1)
-                    $('#year').html(item.year)
+                    $('#title').html('<h4>'+item.name+'('+item.year+') </h4>')
+                    $('#director').html( '감독: '+item.director)
+                    $('#genre').html('장르: '+item.genre1+', '+item.genre2+', '+item.genre3+', '+item.genre4)
+                    $('#actor').html('배우: '+ item.actor1+', '+item.actor2+', '+item.actor3+', '+item.actor4)
+                    $('#blank').html( "1")
+                    $('#story').html( item.stroy)
                 }
 
             });
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,18 @@ var mysql = require('mysql');
 
 //데이터베이스와 연결합니다.
 var connection = mysql.createConnection({
-    user: 'root',
-    password: '1q2w3e',
-    table: 'movieinfo',
-    database: 'movie' //연결할 데이터베이스
+	user: 'root',
+	password: '1q2w3e',
+	table: 'movieinfo',
+	database: 'movie' //연결할 데이터베이스
 });
 
 exports.index = function(req, res){
-  res.render('index');
+	res.render('index');
 };
 
 exports.registerForm = function(req, res){
-  res.render('register-form');
+	res.render('register-form');
 };
 
 exports.loginForm = function(req, res){
@@ -22,60 +22,60 @@ exports.loginForm = function(req, res){
 }
 
 exports.register = function(req, res){
-       var info = {
-            usermail: req.body.useremail,
-            password: req.body.password,
-            password2: req.body.password2
-        }
-       connection.query('SELECT * FROM movieinfo WHERE useremail = ?', [req.body.useremail], function (err, results, fields) {
-    	   //이미 
-           if (results[0] != undefined) {
-               console.log(typeof(results));
-               console.log("이미 가입 되어 있습니다.");//
-           }
-           else {
-               if (info.password == info.password2) {
-            	   //전부 맞을때 sql 입력
-                  connection.query('INSERT INTO  movieinfo SET useremail = ?, password = ?, password2 = ?',
-                     [info.usermail, info.password, info.password2], function (err) {
-                      if (err) {
-                        throw err; 
-                     }
-                     console.log("가입되었습니다. 환영합니다");
-                     //index를 다시 띄움
-                     res.redirect('/');
-                    });
-              }
-               else{
-            	   //입력 비밀번호가 다를때
-                  console.log("비밀번호가 다릅니다.");
-                 console.log(results);
-               	}
-           }
-       });
+	 var info = {
+		usermail: req.body.useremail,
+		password: req.body.password,
+		password2: req.body.password2
+	}
+	 connection.query('SELECT * FROM movieinfo WHERE useremail = ?', [req.body.useremail], function (err, results, fields) {
+		 //이미 
+		 if (results[0] != undefined) {
+			 console.log(typeof(results));
+			 console.log("이미 가입 되어 있습니다.");//
+		 }
+		 else {
+			 if (info.password == info.password2) {
+				 //전부 맞을때 sql 입력
+				connection.query('INSERT INTO  movieinfo SET useremail = ?, password = ?, password2 = ?',
+				 [info.usermail, info.password, info.password2], function (err) {
+					if (err) {
+						throw err; 
+					 }
+					 console.log("가입되었습니다. 환영합니다");
+					 //index를 다시 띄움
+						 res.redirect('/');
+				});
+			}
+			 else{
+				 //입력 비밀번호가 다를때
+				console.log("비밀번호가 다릅니다.");
+				 console.log(results);
+			}
+		 }
+	 });
 };
 
 exports.login = function(req, res){
-    connection.query('SELECT * FROM movieinfo WHERE useremail = ?', [req.body.useremail], function (err, results, fields) {
-        //console.log(req);
-        if (results[0] != undefined) {
-            console.log(results[0].password);
-            console.log(req.body.password);
-            if (results[0].password == req.body.password) {
-                console.log("로그인 가능합니다.");
-                //exports.index다시 실행될때 변수로 넣기 위해 선언
-                req.session.useremail = req.body.useremail;
-                //index를 다시 띄움
-                res.redirect('/user');
-            }
-            else {
-                console.log("비밀번호를 다르게 입력하였습니다.");
-            }
-        }
-        else {
-            console.log("가입되어 있지 않습니다.");
-        }
-    });
+	connection.query('SELECT * FROM movieinfo WHERE useremail = ?', [req.body.useremail], function (err, results, fields) {
+			//console.log(req);
+			if (results[0] != undefined) {
+					console.log(results[0].password);
+					console.log(req.body.password);
+					if (results[0].password == req.body.password) {
+							console.log("로그인 가능합니다.");
+							//exports.index다시 실행될때 변수로 넣기 위해 선언
+							req.session.useremail = req.body.useremail;
+							//index를 다시 띄움
+							res.redirect('/user');
+					}
+					else {
+							console.log("비밀번호를 다르게 입력하였습니다.");
+					}
+			}
+			else {
+					console.log("가입되어 있지 않습니다.");
+			}
+	});
 }
 exports.loginOk = function(req, res){
 	res.render('login');
@@ -91,8 +91,8 @@ exports.mv = function(req, res){
 
 //DB 조회 -json으로 변환 10개씩
 exports.load = function(req, res) {
-    connection.query('SELECT * FROM movie LIMIT 0,10', function(err, rows) {
-        res.send(rows);
-    });
+	connection.query('SELECT * FROM movie LIMIT 0,10', function(err, rows) {
+			res.send(rows);
+	});
 };
-                         
+												 
