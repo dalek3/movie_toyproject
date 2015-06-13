fc0985b56932c565122bd3ff907f623d43b306d4
Jang <lina1318@gmail.com>
2015-06-14 00:44:23 +0900

    회원가입 버그 수정

--- routes/index.js
+++ routes/index.js
@@ -46,16 +46,15 @@ exports.register = function(req, res){
 		password: req.body.password,
 		password2: req.body.password2
 	}
-	 connection.query('SELECT * FROM movieinfo WHERE username = ?', info.username, function (err, results) {
-		 if (info.username === results[0].username) {
-			 console.log("이미 가입 되어 있습니다.");
-			 res.redirect('/'); 
-		 }
-		 else {
-	 	 	if (info.password === info.password2) {
-				 //전부 맞을때 sql 입력
-				connection.query('INSERT INTO  movieinfo SET username = ?, password = ?',
-				 [info.username , info.password], function (err) {
+	connection.query('SELECT * FROM movieinfo WHERE username = ?', info.username, function (err,results) {
+		if (results[0] !== undefined) {
+			console.log("이미 가입 되어 있습니다.");
+			res.redirect('/register'); 
+		}
+		else{
+			if(info.password === info.password2) {
+				//전부 맞을때 sql 입력
+				connection.query('INSERT INTO  movieinfo SET username = ?, password = ?',[info.username , info.password], function (err) {
 					console.log("가입되었습니다. 환영합니다");
 					//exports.index다시 실행될때 변수로 넣기 위해 선언
 					req.session.username = info.username;
@@ -66,32 +65,30 @@ exports.register = function(req, res){
 			else{
 				 //입력 비밀번호가 다를때
 				console.log("비밀번호가 다릅니다.");
-				console.log(results);
-				res.redirect('/'); 
+				res.redirect('/register'); 
 			}
-		 }
-	 });
+		}
+	});
 };
 
 exports.login = function(req, res){
-	connection.query('SELECT * FROM movieinfo WHERE username = ?', [req.body.username], function (err, results, fields) {
+	connection.query('SELECT * FROM movieinfo WHERE username = ?', [req.body.username], function (err, results) {
 		if (results[0] !== undefined) {
-				console.log(results[0].password);
-				console.log(req.body.password);
-				console.log(results[0].username);
-				if (results[0].password == req.body.password) {
-						console.log("로그인 가능합니다.");
-						//exports.index다시 실행될때 변수로 넣기 위해 선언
-						req.session.username = results[0].username;
-						//index를 다시 띄움
-						res.redirect(req.session.username);
-				}
-				else {
-						console.log("비밀번호를 다르게 입력하였습니다.");
-				}
+			if (results[0].password === req.body.password) {
+				console.log("로그인 가능합니다.");
+				//exports.index다시 실행될때 변수로 넣기 위해 선언
+				req.session.username = results[0].username;
+				//index를 다시 띄움
+				res.redirect(req.session.username);
+			}
+			else {
+					console.log("비밀번호를 다르게 입력하였습니다.");
+					res.redirect('/login'); 
+			}
 		}
 		else {
 				console.log("가입되어 있지 않습니다.");
+				res.redirect('/login'); 
 		}
 	});
 }
@@ -104,7 +101,6 @@ exports.loginOk = function(req, res){
 		});
 	});
 }
-
 
 exports.userinfoform = function (req, res) { //좋아요 영화 쿼리
     //var favorlite;
@@ -123,10 +119,6 @@ exports.passwordchangeform = function (req, res) {
     res.render('password');
 };
 
-exports.withdrawalform = function (req, res) {
-    res.render('withdrawal');
-}
-
 exports.passwordchange = function (req, res) {
 	var data = req.session.username;
 	if (req.body.confirm === req.body.new) {
@@ -140,12 +132,16 @@ exports.passwordchange = function (req, res) {
 	}
 };
 
+exports.withdrawalform = function (req, res) {
+    res.render('withdrawal');
+}
+
 exports.withdrawal = function (req, res) {
 	var data = req.session.username;
-		connection.query('DELETE from movieinfo WHERE username = ?', data,function (err){
-			console.log(data);
-			res.redirect('/'); 
-   		 });
+	connection.query('DELETE from movieinfo WHERE username = ?', data,function (err){
+		console.log(data);
+		res.redirect('/'); 
+	});
 }
 // 안됨
 exports.logout = function(req, res){
