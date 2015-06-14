4e50b5639298e0da79a49e87008b382de6c34367
Jang <lina1318@gmail.com>
2015-06-14 10:04:44 +0900

    no message

--- public/js/grid.js
+++
@@ -1,7 +0,0 @@
-$
-.grid
- each item in row
-  .grid-item
-   a(href='/movie/#{item.name}' data-toggle='modal' data-target='#myModal')
-    img(src='#{item.imgpath}')
-
--- public/js/script.js
+++ public/js/script.js
@@ -3,27 +3,6 @@ $(function () {
 
 	$('.grid-item').imagesLoaded(function () {
 		$container.isotope({ });
-		/*$container.infinitescroll({
-			navSelector: 'a#next:last', // selector for the paged navigation 
-			nextSelector: 'a#next:last', // selector for the NEXT link (to page 2)
-			itemSelector: '.grid-item', // selector for all items you'll retrieve
-			dataType: 'json',
-			debug: true,
-			appendCallback	: false // USE FOR PREPENDING
-		},
-		function( response ) {
-			var jsonData = response.results;
-			var html = '';
-			$.each(jsonData, function (index, item) {
-				html += '<div class="grid-item">';
-				html += '<a class="thumbnail">'
-				html += '<img src="' + item.title + '">';
-				html += '</a>'
-				html += '</div>';
-			});
-			$container.append(html).isotope('appended', $('.grid-item'))
-			
-	      });*/
 	});
 	$('#myModal').on('hidden.bs.modal',function(){
 		$(this).removeData('bs.modal')
